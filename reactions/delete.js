5e6870ec9627f48b26bd9ad3a52fde192f35ec7e
Trong Nghia Hoang <zelgiushoang@gmail.com>
2021-07-21 22:02:24 +0700

    final

--- reactions/delete.js
+++ reactions/delete.js
@@ -1,50 +1,49 @@
-const Discord = require('discord.js')
-const mongo = require('../src/connect')
-const delay = require('delay')
+const Discord = require("discord.js");
+const mongo = require("../src/connect");
+const delay = require("delay");
 
-function ticketMessage(){
-    return new Discord.MessageEmbed()
-        .setColor('#bbf1c8')
-        .setTitle('**Ticket Delete**')
-        .setDescription(`This ticket will be deleted in 5 seconds`)
-        .setTimestamp()
+function ticketMessage() {
+  return new Discord.MessageEmbed()
+    .setColor("#bbf1c8")
+    .setTitle("**Ticket Delete**")
+    .setDescription(`Ticket sẽ bị xóa sau 5 giây`)
+    .setTimestamp();
 }
 
-function Wrong(auID){
-    return new Discord.MessageEmbed()
-        .setColor('#28df99')
-        .setDescription(`<@${auID}> Something Went wrong. Please Try again`)
-        .setTimestamp()
+function Wrong(auID) {
+  return new Discord.MessageEmbed()
+    .setColor("#28df99")
+    .setDescription(`<@${auID}> Something Went wrong. Please Try again`)
+    .setTimestamp();
 }
 
-function ticket_delete(message,user){
-    mongo.validateTicket_Channel(message.channel.id,(result)=>{
-        if(result){
-            message.channel.send(ticketMessage()).then(async msg=>{
-                await delay(5000).then(m=>{
-                    mongo.deleteTicket(msg.channel.id,(res)=>{
-                        if(res){
-                            mongo.deleteTicketPanel(msg.channel.id,(res1)=>{
-                                if(res1){
-                                    msg.channel.delete().then(m1=>{
-                                    }).catch(err=>{
-                                        throw err
-                                    })
-                                }
-                                else{
-                                    return message.channel.send(Wrong(user.id))
-                                }
-                            })
-                        }
-                        else{
-                            return message.channel.send(Wrong(user.id))
-                        }
-                    })
-                    
-                })
-            })
-        }
-    })
+function ticket_delete(message, user) {
+  mongo.validateTicket_Channel(message.channel.id, (result) => {
+    if (result) {
+      message.channel.send(ticketMessage()).then(async (msg) => {
+        await delay(5000).then((m) => {
+          mongo.deleteTicket(msg.channel.id, (res) => {
+            if (res) {
+              mongo.deleteTicketPanel(msg.channel.id, (res1) => {
+                if (res1) {
+                  msg.channel
+                    .delete()
+                    .then((m1) => {})
+                    .catch((err) => {
+                      throw err;
+                    });
+                } else {
+                  return message.channel.send(Wrong(user.id));
+                }
+              });
+            } else {
+              return message.channel.send(Wrong(user.id));
+            }
+          });
+        });
+      });
+    }
+  });
 }
 
-module.exports = { ticket_delete }
+module.exports = { ticket_delete };
