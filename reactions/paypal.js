af445e387af03453b8400cfade57c250524ea243
Trong Nghia Hoang <zelgiushoang@gmail.com>
2021-07-21 22:37:27 +0700

    fix shit

--- reactions/paypal.js
+++ reactions/paypal.js
@@ -1,87 +1,91 @@
-const Discord = require('discord.js')
-const mongo = require('../src/connect')
-const { paypal_email } = require('../config.json')
+const Discord = require("discord.js");
+const mongo = require("../src/connect");
+const { paypal_email } = require("../config.json");
 
-function ticketMessage(id){
-    return new Discord.MessageEmbed()
-        .setColor('#bbf1c8')
-        .setTitle('**WHitelist Ticket**')
-        .setDescription(`Hello <@${id}> ,\n\ntest`)
-        .setTimestamp()
+function ticketMessage(id) {
+  return new Discord.MessageEmbed()
+    .setColor("#bbf1c8")
+    .setTitle("**WHitelist Ticket**")
+    .setDescription(`Hello <@${id}> ,\n\ntest`)
+    .setTimestamp();
 }
 
-function SpamTicket(auID,chID){
-    return new Discord.MessageEmbed()
-        .setColor('#28df99')
-        .setDescription(`<@${auID}> You've Already a Ticket opened at <#${chID}>`)
-        .setTimestamp()
+function SpamTicket(auID, chID) {
+  return new Discord.MessageEmbed()
+    .setColor("#28df99")
+    .setDescription(`<@${auID}> You've Already a Ticket opened at <#${chID}>`)
+    .setTimestamp();
 }
 
-function verify_closed(res){
-    var response = {
-        status: false,
-        channel: null
+function verify_closed(res) {
+  var response = {
+    status: false,
+    channel: null,
+  };
+  for (data of res) {
+    if (data.status !== "closed") {
+      response.status = true;
+      response.channel = data.channelID;
+      break;
     }
-    for(data of res){
-        if(data.status !== 'closed'){
-            response.status = true
-            response.channel = data.channelID
-            break
-        }
-    }
-    return response
+  }
+  return response;
 }
 
-function paypal_ticket(message,user){
-    mongo.validateTicket_Author(user.id,async (res)=>{
-        try{
-            status = verify_closed(res)
-            if(status.status === true){
-                return user.send(SpamTicket(user.id,status.channel))
-            }
-            else{
-                await message.guild.channels.create(`whitelist-${user.username}`, {
-                    type: 'text', 
-                    permissionOverwrites: [
-                        {
-                            id: message.guild.id,
-                            deny: ['VIEW_CHANNEL'],
-                        },
-                        {
-                            id: user.id,
-                            allow: ['VIEW_CHANNEL'],
-                        },
-                    ],
-                }).then(channel=>{
-                    mongo.validateConfig(message.guild.id,(r)=>{
-                        if(r){
-                            if(r.support.roles){
-                                let roles = r.support.roles.split(',')
-                                for(let role of roles){
-                                    channel.updateOverwrite(role, { VIEW_CHANNEL: true})
-                                }
-                            }
-    
-                            channel.send(`<@${user.id}>`)
-                            channel.send(ticketMessage(user.id)).then(async msg=>{
-                                await msg.react('🔒').then(m => {
-                                    mongo.newTicket(msg.guild.id,user.id,channel.id,msg.id,(result)=>{
-                                        if(result){
-                                            console.log('New Ticket Created Successfully')
-                                        }
-                                    })
-                                })
-                            })
+function paypal_ticket(message, user) {
+  mongo.validateTicket_Author(user.id, async (res) => {
+    try {
+      status = verify_closed(res);
+      if (status.status === true) {
+        return user.send(SpamTicket(user.id, status.channel));
+      } else {
+        await message.guild.channels
+          .create(`whitelist-${user.username}`, {
+            type: "text",
+            parent: "863661967091826699",
+            permissionOverwrites: [
+              {
+                id: message.guild.id,
+                deny: ["VIEW_CHANNEL"],
+              },
+              {
+                id: user.id,
+                allow: ["VIEW_CHANNEL"],
+              },
+            ],
+          })
+          .then((channel) => {
+            mongo.validateConfig(message.guild.id, (r) => {
+              if (r) {
+                if (r.support.roles) {
+                  let roles = r.support.roles.split(",");
+                  for (let role of roles) {
+                    channel.updateOverwrite(role, { VIEW_CHANNEL: true });
+                  }
+                }
+
+                channel.send(`<@${user.id}>`);
+                channel.send(ticketMessage(user.id)).then(async (msg) => {
+                  await msg.react("🔒").then((m) => {
+                    mongo.newTicket(
+                      msg.guild.id,
+                      user.id,
+                      channel.id,
+                      msg.id,
+                      (result) => {
+                        if (result) {
+                          console.log("New Ticket Created Successfully");
                         }
-                    })
-                })
-            }
-        }
-        catch(error){
-
-        }
-        
-    })
+                      }
+                    );
+                  });
+                });
+              }
+            });
+          });
+      }
+    } catch (error) {}
+  });
 }
 
-module.exports = { paypal_ticket }
+module.exports = { paypal_ticket };
